d720b78ca7bc12d6609d1d7de3a1bbf862347576
Vivek Kumar <vivek.kumar@avizva.com>
2020-06-01 17:54:24 +0530

    test

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { router } = require('./routes');
 const { DB } = require('./constants');
 const { configure } = require('test-worker-module');
-
-const PORT = process.env.PORT || 3000;
-const app = express();
-app.use(bodyParser.json());
-app.use(router);
-app.listen(PORT);
 
 (async () => {
 	await configure({
@@ -16,5 +7,3 @@ app.listen(PORT);
 		dbName: DB.DB_NAME
 	});
 })();
-
-console.log(`Server started at http://localhost:${PORT}`);
